30602db12b56848f26d353964e424488421eac6a
Daniel <dany19992212@gmail.com>
2025-01-15 10:21:23 -0500

    Two components, Text and Title2, were added, and AboutMe was created.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
 import vercelAdapter from '@astrojs/vercel';
 
 // https://astro.build/config
