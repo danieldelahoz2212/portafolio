702f7d171b4d9c2f042cc4a70f7df9131a0344f9
Daniel <dany19992212@gmail.com>
2025-01-15 20:29:39 -0500

    Netlify dependencies were added in the astro.config.mjs file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelAdapter from '@astrojs/vercel';
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind()],
-  
-    output: "server",
-    adapter: vercelAdapter(),
-  });
+  integrations: [tailwind()],
+
+  output: "server",
+  adapter: netlify(),
+});
